e80d072d4113069e5669dde60e7fab25b08e94f3
kalvin.l <kalvin.dev@pm.me>
2022-01-22 20:22:48 +0800

    extra eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
     root: true,
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: [
+        "eslint:recommended",
+        "plugin:eslint-plugin/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
     ignorePatterns: ["/.nyc_output", "/coverage", "/dist"],
     rules: {
         // Use self.
